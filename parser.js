8723f34e5d6bf581e72beac2a50dae569c29372b
posadsky84 <a.i.posadsky@gmail.com>
2024-06-12 19:34:05 +0300

    Развитие парсера (много мелочей)

--- parser.js
+++ parser.js
@@ -1,52 +1,556 @@
 const dataMap = [
+
+  { raceName: "Весенний Турмарафон 2017",
+    id: 1,
+    ddate: '2017-04-22',
+    needLoad: false,
+    sname: "Весенний",
+    location: "Ерденево",
+    distances: [
+
+    ],
+  },
+  { raceName: "Летний Турмарафон 2017",
+    id: 2,
+    ddate: '2017-07-22',
+    needLoad: false,
+    sname: "Летний",
+    location: "Шаховская",
+    distances: [
+
+    ],
+  },
+  { raceName: "Осенний Турмарафон 2017",
+    id: 3,
+    ddate: '2017-10-07',
+    needLoad: false,
+    sname: "Осенний",
+    location: "Конев бор",
+    distances: [
+
+    ],
+  },
+  { raceName: "Зимний Турмарафон 2017",
+    id: 4,
+    ddate: '2017-12-09',
+    needLoad: false,
+    sname: "Зимний",
+    location: "Заокский",
+    distances: [
+
+    ],
+  },
+  { raceName: "Весенний Турмарафон 2018",
+    id: 5,
+    ddate: '2018-04-14',
+    needLoad: false,
+    sname: "Весенний",
+    location: "Балакирево",
+    distances: [
+
+    ],
+  },
+  { raceName: "Летний Турмарафон 2018",
+    id: 6,
+    ddate: '2018-07-15',
+    needLoad: false,
+    sname: "Весенний",
+    location: "Кузяево",
+    distances: [
+
+    ],
+  },
+  { raceName: "Осенний Турмарафон 2018",
+    id: 7,
+    ddate: '2018-10-06',
+    needLoad: false,
+    sname: "Осенний",
+    location: "Верея",
+    distances: [
+
+    ],
+  },
+  { raceName: "Зимний Турмарафон 2018",
+    id: 8,
+    ddate: '2018-12-08',
+    needLoad: false,
+    sname: "Зимний",
+    location: "Лесодолгоруково",
+    distances: [
+
+    ],
+  },
+  { raceName: "Весенний Турмарафон 2019",
+    id: 9,
+    ddate: '2019-04-20',
+    needLoad: false,
+    sname: "Весенний",
+    location: "Узуново",
+    distances: [
+
+    ],
+  },
+  { raceName: "Летний Турмарафон 2019",
+    id: 10,
+    ddate: '2019-07-20',
+    needLoad: false,
+    sname: "Летний",
+    location: "Сушнево",
+    distances: [
+
+    ],
+  },
+  { raceName: "Осенний Турмарафон 2019",
+    id: 11,
+    ddate: '2019-10-05',
+    needLoad: false,
+    sname: "Осенний",
+    location: "Донховка",
+    distances: [
+
+    ],
+  },
+  { raceName: "Зимний Турмарафон 2019",
+    id: 12,
+    ddate: '2019-12-07',
+    needLoad: false,
+    sname: "Зимний",
+    location: "Шаликово",
+    distances: [
+
+    ],
+  },
+  { raceName: "Лыжный Турмарафон 2020",
+    id: 13,
+    ddate: '2020-02-15',
+    needLoad: false,
+    sname: "Зимний",
+    location: "Лесодолгоруково",
+    distances: [
+
+    ],
+  },
+  { raceName: "Весенний Турмарафон 2020",
+    id: 14,
+    ddate: '2020-04-18',
+    needLoad: false,
+    sname: "Весенний",
+    location: "Жилёво",
+    distances: [
+
+    ],
+  },
+  { raceName: "Летний Турмарафон 2020",
+    id: 15,
+    ddate: '2020-07-18',
+    needLoad: false,
+    sname: "Летний",
+    location: "Хотьково",
+    distances: [
+
+    ],
+  },
+  { raceName: "Осенний Турмарафон 2020",
+    id: 16,
+    ddate: '2020-10-03',
+    needLoad: false,
+    sname: "Осенний",
+    location: "Воиново",
+    distances: [
+
+    ],
+  },
+  { raceName: "Зимний Турмарафон 2020",
+    id: 17,
+    ddate: '2020-12-12',
+    needLoad: false,
+    sname: "Зимний",
+    location: "Ямуга",
+    distances: [
+
+    ],
+  },
+  { raceName: "Лыжный Турмарафон 2021",
+    id: 18,
+    ddate: '2021-02-13',
+    needLoad: false,
+    sname: "Лыжный",
+    location: "Балакирево",
+    distances: [
+
+    ],
+  },
+  { raceName: "Весенний Турмарафон 2021",
+    id: 19,
+    ddate: '2021-04-17',
+    needLoad: false,
+    sname: "Весенний",
+    location: "Суходрев",
+    distances: [
+
+    ],
+  },
+  { raceName: "наСТОящий Турмарафон 2021",
+    id: 20,
+    ddate: '2021-05-29',
+    needLoad: false,
+    sname: "наСТОящий",
+    location: "Петушки-Черусти",
+    distances: [
+
+    ],
+  },
+  { raceName: "Летний Турмарафон 2021",
+    id: 21,
+    ddate: '2021-07-17',
+    needLoad: false,
+    sname: "Летний",
+    location: "Жилёво",
+    distances: [
+
+    ],
+  },
+  { raceName: "Осенний Турмарафон 2021",
+    id: 22,
+    ddate: '2021-10-02',
+    needLoad: false,
+    sname: "Осенний",
+    location: "Москворецкая",
+    distances: [
+
+    ],
+  },
+  { raceName: "Зимний Турмарафон 2021",
+    id: 23,
+    ddate: '2021-12-11',
+    needLoad: false,
+    sname: "Зимний",
+    location: "Темпы",
+    distances: [
+
+    ],
+  },
+  { raceName: "Весенний Турмарафон 2022",
+    id: 24,
+    ddate: '2022-04-23',
+    needLoad: false,
+    sname: "Весенний",
+    location: "Боровск",
+    distances: [
+      {fullName: "Весенний ТМ 2022, 25km Lite",
+        name: "25km Lite",
+        courseType: "lite",
+        km: 25,
+        fileName: "весна2022_25.txt"
+      },
+      {fullName: "Весенний ТМ 2022, 50km Tourist",
+        name: "50km Tourist",
+        courseType: "tourist",
+        km: 50,
+        fileName: "весна2022_50.txt",
+      },
+      {fullName: "Весенний ТМ 2022, 75km Ultra",
+        name: "75km Ultra",
+        courseType: "ultra",
+        km: 75,
+        fileName: "весна2022_75.txt",
+      },
+    ],
+  },
+  { raceName: "Летний Турмарафон 2022",
+    id: 25,
+    ddate: '2022-07-16',
+    needLoad: false,
+    sname: "Летний",
+    location: "Грибово",
+    distances: [
+      {fullName: "Летний ТМ 2022, 30km Lite",
+        name: "30km Lite",
+        courseType: "lite",
+        km: 30,
+        fileName: "лето2022_30.txt"
+      },
+      {fullName: "Летний ТМ 2022, 55km Tourist",
+        name: "55km Tourist",
+        courseType: "tourist",
+        km: 55,
+        fileName: "лето2022_55.txt",
+      },
+    ],
+  },
+  { raceName: "Осенний Турмарафон 2022",
+    id: 26,
+    ddate: '2022-10-08',
+    needLoad: false,
+    sname: "Осенний",
+    location: "Звенигород",
+    distances: [
+      {fullName: "Осенний ТМ 2022, 30km Lite",
+        name: "30km Lite",
+        courseType: "lite",
+        km: 30,
+        fileName: "осень2022_30.txt"
+      },
+      {fullName: "Осенний ТМ 2022, 50km Tourist",
+        name: "50km Tourist",
+        courseType: "tourist",
+        km: 50,
+        fileName: "осень2022_50.txt",
+      },
+      {fullName: "Осенний ТМ 2022, 75km Ultra",
+        name: "75km Ultra",
+        courseType: "ultra",
+        km: 75,
+        fileName: "осень2022_75.txt",
+      },
+    ],
+  },
+  { raceName: "Зимний Турмарафон 2022",
+    id: 27,
+    ddate: '2022-12-10',
+    needLoad: false,
+    sname: "Зимний",
+    location: "Запутная",
+    distances: [
+      {fullName: "Зимний ТМ 2022, 25km Lite",
+        name: "25km Lite",
+        courseType: "lite",
+        km: 25,
+        fileName: "зима2022_25.txt"
+      },
+      {fullName: "Зимний ТМ 2022, 50km Tourist",
+        name: "50km Tourist",
+        courseType: "tourist",
+        km: 50,
+        fileName: "зима2022_50.txt",
+      },
+    ],
+  },
   { raceName: "Лыжный Турмарафон 2023",
-    season: 2023,
-    needLoad: true,
+    id: 28,
+    ddate: '2023-02-18',
+    needLoad: false,
+    sname: "Лыжный",
+    location: "Донховка",
     distances: [
       {fullName: "Лыжный ТМ 2023, 20km Lite",
         name: "25km Lite",
+        courseType: "lite",
+        km: 25,
         fileName: "лыжный2023_20.txt"
       },
       {fullName: "Лыжный Турмарафон 2023 30km Tourist",
         name: "30km Tourist",
+        courseType: "tourist",
+        km: 30,
         fileName: "лыжный2023_30.txt",
       },
     ],
   },
-  { raceName: "Весенний Турмарафон 2024",
-    season: 2024,
-    needLoad: true,
+  { raceName: "Весенний Турмарафон 2023",
+    id: 29,
+    ddate: '2023-04-22',
+    needLoad: false,
+    sname: "Весенний",
+    location: "Пущино",
     distances: [
-      {fullName: "Весенний ТМ 2024, 25km Lite",
+      {fullName: "Весенний ТМ 2023, 25km Lite",
         name: "25km Lite",
-        fileName: "весна2024_25.txt"
+        courseType: "lite",
+        km: 25,
+        fileName: "весна2023_25.txt"
       },
-      {fullName: "Весенний ТМ 2024, 50km Tourist",
+      {fullName: "Весенний ТМ 2023, 50km Tourist",
         name: "50km Tourist",
-        fileName: "весна2024_50.txt",
+        courseType: "tourist",
+        km: 50,
+        fileName: "весна2023_50.txt",
       },
-      {fullName: "Весенний ТМ 2024, 75km Ultra",
+      {fullName: "Весенний ТМ 2023, 75km Ultra",
         name: "75km Ultra",
-        fileName: "весна2024_75.txt",
+        courseType: "ultra",
+        km: 75,
+        fileName: "весна2023_75.txt",
+      },
+    ],
+  },
+  { raceName: "наСТОящий Турмарафон 2023",
+    id: 30,
+    ddate: '2023-05-27',
+    needLoad: false,
+    sname: "наСТОящий",
+    location: "Белоомут - Шатурторф",
+    distances: [
+      {fullName: "наСТОящий ТМ 2023, 100km",
+        name: "100km",
+        courseType: "real",
+        km: 100,
+        fileName: "настоящий2023.txt"
+      },
+
+    ],
+  },
+  { raceName: "Летний Турмарафон 2023",
+    id: 31,
+    ddate: '2023-07-15',
+    needLoad: false,
+    sname: "Летний",
+    location: "Погорелое Городище",
+    distances: [
+      {fullName: "Летний ТМ 2023, 25km Lite",
+        name: "25km Lite",
+        courseType: "lite",
+        km: 25,
+        fileName: "лето2023_25.txt"
+      },
+      {fullName: "Летний ТМ 2023, 50km Tourist",
+        name: "50km Tourist",
+        courseType: "tourist",
+        km: 50,
+        fileName: "лето2023_50.txt",
+      },
+    ],
+  },
+  { raceName: "Осенний Турмарафон 2023",
+    id: 32,
+    ddate: '2023-10-14',
+    needLoad: false,
+    sname: "Осенний",
+    location: "Вербилки",
+    distances: [
+      {fullName: "Осенний ТМ 2023, 25km Lite",
+        name: "25km Lite",
+        courseType: "lite",
+        km: 25,
+        fileName: "осень2023_25.txt"
+      },
+      {fullName: "Осенний ТМ 2023, 50km Tourist",
+        name: "50km Tourist",
+        courseType: "tourist",
+        km: 50,
+        fileName: "осень2023_50.txt",
       },
     ],
   },
   { raceName: "Зимний Турмарафон 2023",
-    season: 2023,
+    id: 33,
+    ddate: '2023-12-09',
     needLoad: false,
+    sname: "Зимний",
+    location: "Башкино",
     distances: [
       {fullName: "Зимний Турмарафон 2023 25km Lite",
         name: "25km Lite",
+        courseType: "lite",
+        km: 25,
         fileName: "зима2023_25.txt"
       },
       {fullName: "Зимний Турмарафон 2023 55km Tourist",
         name: "55km Tourist",
+        courseType: "tourist",
+        km: 55,
         fileName: "зима2023_55.txt",
       },
     ],
   },
+  { raceName: "Лыжный Турмарафон 2024",
+    id: 34,
+    ddate: '2024-02-17',
+    needLoad: false,
+    sname: "Лыжный",
+    location: "Запутная",
+    distances: [
+      {fullName: "Лыжный ТМ 2024, 20km Lite",
+        name: "20km Lite",
+        courseType: "lite",
+        km: 20,
+        fileName: "лыжный2024_20.txt"
+      },
+      {fullName: "Лыжный ТМ 2024, 40km Tourist",
+        name: "40km Tourist",
+        courseType: "tourist",
+        km: 40,
+        fileName: "лыжный2024_40.txt",
+      },
+    ],
+  },
+  { raceName: "Весенний Турмарафон 2024",
+    id: 35,
+    ddate: '2024-04-20',
+    needLoad: true,
+    sname: "Весенний",
+    location: "Шеметово (Ясногорск)",
+    distances: [
+      {fullName: "Весенний ТМ 2024, 25km Lite",
+        name: "25km Lite",
+        courseType: "lite",
+        km: 25,
+        fileName: "весна2024_25.txt"
+      },
+      {fullName: "Весенний ТМ 2024, 50km Tourist",
+        name: "50km Tourist",
+        courseType: "tourist",
+        km: 50,
+        fileName: "весна2024_50.txt",
+      },
+      {fullName: "Весенний ТМ 2024, 75km Ultra",
+        name: "75km Ultra",
+        courseType: "ultra",
+        km: 75,
+        fileName: "весна2024_75.txt",
+      },
+    ],
+  },
+  { raceName: "наСТОящий Турмарафон 2024",
+    id: 36,
+    ddate: '2024-06-01',
+    needLoad: false,
+    sname: "наСТОящий",
+    location: "Головино - Шатурторф",
+    distances: [
+      {fullName: "наСТОящий ТМ 2024, 100km",
+        name: "100km",
+        courseType: "real",
+        km: 100,
+        fileName: "настоящий2024.txt"
+      },
+    ],
+  },
+  { raceName: "Летний Турмарафон 2024",
+    id: 37,
+    ddate: '2024-07-13',
+    needLoad: false,
+    sname: "Летний",
+    location: "",
+    distances: [
+
+    ],
+  },
+  { raceName: "Осенний Турмарафон 2024",
+    id: 38,
+    ddate: '2024-10-05',
+    needLoad: false,
+    sname: "Осенний",
+    location: "",
+    distances: [
+
+    ],
+  },
+  { raceName: "Зимний Турмарафон 2024",
+    id: 39,
+    ddate: '2024-12-07',
+    needLoad: false,
+    sname: "Зимний",
+    location: "",
+    distances: [
+
+    ],
+  },
+
+
 
 ];
+
 
 
 
@@ -70,12 +574,15 @@ instance.interceptors.request.use(req => {
 
 
 
-const postRace = async ({raceName, season}) => {
+const postRace = async ({id, raceName, ddate, sname, location}) => {
 
   const raceData = {
+    id,
     data: {
       name: raceName,
-      season,
+      ddate,
+      sname,
+      location,
     }
   }
 
@@ -95,13 +602,15 @@ const postRace = async ({raceName, season}) => {
 }
 
 
-const postDistance = async ({fullName, name, raceId}) => {
+const postDistance = async ({fullName, name, raceId, km, courseType}) => {
 
   const distanceData = {
     data: {
       fullName,
       name,
       race: raceId,
+      km,
+      courseType
     }
   }
 
@@ -320,39 +829,50 @@ const fLoadRace = async (race) => {
   for (let distance of race.distances) {
 
     console.log("Загружаем дистанцию: " + distance.name);
-    const distanceId = await postDistance({fullName: distance.fullName, name: distance.name, raceId});
-    const data = fs.readFileSync("data/" + distance.fileName).toString().split(`\n`).map(i => i.split(`\t`));
+    const distanceId = await postDistance(
+      {fullName: distance.fullName,
+        name: distance.name,
+        raceId,
+        km: distance.km,
+        courseType: distance.courseType,
+      });
 
-    for (const item of data) {
+    if (race.needLoad) {
 
-      let runners = [];
-      let runnersChildren = [];
+      const data = fs.readFileSync("data/" + distance.fileName).toString().split(`\n`).map(i => i.split(`\t`));
 
-      let res;
-      res = item[2] ? parseRunner(item[2]) : null;
-      if (res) await postRunner(res, runners);
-      res = item[3] ? parseRunner(item[3]) : null;
-      if (res) await postRunner(res, runners);
-      res = item[4] ? parseRunner(item[4]) : null;
-      if (res) await postRunner(res, runners);
-      res = item[5] ? parseKids(item[5]) : [];
-      if (res.length) {
-        for (const item2 of res) await postRunner(item2, runnersChildren);
+      for (const item of data) {
+
+        let runners = [];
+        let runnersChildren = [];
+
+        let res;
+        res = item[2] ? parseRunner(item[2]) : null;
+        if (res) await postRunner(res, runners);
+        res = item[3] ? parseRunner(item[3]) : null;
+        if (res) await postRunner(res, runners);
+        res = item[4] ? parseRunner(item[4]) : null;
+        if (res) await postRunner(res, runners);
+        res = item[5] ? parseKids(item[5]) : [];
+        if (res.length) {
+          for (const item2 of res) await postRunner(item2, runnersChildren);
+        }
+
+        let teamData = {};
+
+        teamData.place = item[0];
+        teamData.name = item[1] ? item[1] : null;
+        teamData.start = item[6] ? myToDate(item[6]) : null;
+        teamData.finish = item[7] ? myToDate(item[7]) : null;
+        teamData.comm = item[9] ? item[9] : null;
+        teamData.runners = runners;
+        teamData.runnersChildren = runnersChildren;
+        teamData.distance = distanceId;
+
+        await postTeam(teamData);
+
+
       }
-
-      let teamData = {};
-
-      teamData.place = item[0];
-      teamData.name = item[1] ? item[1] : null;
-      teamData.start = item[6] ? myToDate(item[6]) : null;
-      teamData.finish = item[7] ? myToDate(item[7]) : null;
-      teamData.comm = item[9] ? item[9] : null;
-      teamData.runners = runners;
-      teamData.runnersChildren = runnersChildren;
-      teamData.distance = distanceId;
-
-      await postTeam(teamData);
-
 
     }
 
@@ -365,14 +885,13 @@ const fLoadRace = async (race) => {
   };
 
 
+
 }
 
 const fLoadAll = async () => {
 
   for (let item of dataMap) {
-    if (item.needLoad) {
       await fLoadRace(item);
-    }
   }
 
 }
